54b3ff4f863f7d930d4c369101a5c205fa39b70d
jonia89 <jonianttila89@hotmail.com>
2025-09-30 14:42:10 +0300

    3.16 error handling in middleware

--- osio_3/tehtava_3_1-3_22/index.js
+++ osio_3/tehtava_3_1-3_22/index.js
@@ -13,7 +13,9 @@ app.use(
     origin: "https://fullstack-website-e7px.onrender.com",
   })
 );
+app.use(express.static("dist"));
 app.use(express.json());
+app.use(requestLogger);
 
 app.use(
   morgan("tiny", {
@@ -58,34 +60,38 @@ app.use(
 // })
 
 // const generateId = () => {
-  // let id = Math.floor(Math.random() * 1000000).toString();
-  // if (persons.find((person) => person.id === id)) {
-  //   return generateId();
-  // }
-  // return id;
+// let id = Math.floor(Math.random() * 1000000).toString();
+// if (persons.find((person) => person.id === id)) {
+//   return generateId();
+// }
+// return id;
 //   return Math.floor(Math.random() * 1000000).toString();
 // };
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
   // response.json(persons);
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
   // const id = request.params.id;
   // const person = persons.find((person) => person.id === id);
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -104,36 +110,58 @@ app.post("/api/persons", (request, response) => {
     // id: generateId(),
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 
   // persons = persons.concat(person);
   // response.json(person);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end();
-  });
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
   // const id = request.params.id;
   // persons = persons.filter((person) => person.id !== id);
 
   // response.status(204).end();
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response,next) => {
   Person.countDocuments({}).then((count) => {
     const date = new Date();
     response.send(
       `<p>Phonebook has info for ${count} people</p><p>${date}</p>`
     );
-  });
+  }).catch((error) => next(error));
   // const date = new Date();
   // response.send(
   //   `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
   // );
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
