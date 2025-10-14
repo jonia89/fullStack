4b0a5416c65e0eebed40ed2e2c5920abfe2e0700
jonia89 <jonianttila89@hotmail.com>
2025-10-14 14:27:20 +0300

    4.4 tests

--- osio_4/tehtava_4_1-4_23/backend/tests/list_helper.test.js
+++ osio_4/tehtava_4_1-4_23/backend/tests/list_helper.test.js
@@ -1,10 +1,54 @@
-const { test, describe } = require('node:test')
-const assert = require('node:assert')
-const listHelper = require('../utils/list_helper')
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
 
-test('dummy returns one', () => {
-  const blogs = []
+test("dummy returns one", () => {
+  const blogs = [];
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listWithTwoBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Test blog",
+      author: "J.S. Tester",
+      url: "http://www.test.html",
+      likes: 1,
+      __v: 0,
+    },
+  ];
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+    test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs);
+    assert.strictEqual(result, 6);
+  });
+});
