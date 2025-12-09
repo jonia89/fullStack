424a8fe036e7136475c0fa2fbe10decaa17d3e07
jonia89 <jonianttila89@hotmail.com>
2025-12-09 09:57:23 +0200

    4.20

--- osio_4/tehtava_4_1-4_23/backend/app.js
+++ osio_4/tehtava_4_1-4_23/backend/app.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+app.use(middleware.tokenExtractor)
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter)
